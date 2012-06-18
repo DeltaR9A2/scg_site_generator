590c429719c42ac0621e1832903c1744b6d380c9
John Colburn <they.called.him.crazy@gmail.com>
2012-06-18 04:25:22 -0500

    Removed a couple irrelevant scripts.

--- rsync_ev.sh
+++
@@ -1,1 +0,0 @@
-rsync -e ssh -av --delete-during "/home/fearless/Output/Evanished/" thetgartist@evanished.com:~/evanished.com/
--- rsync_nn.sh
+++
@@ -1,1 +0,0 @@
-rsync -e ssh -av --delete-during "/home/fearless/Output/Ninmu Nanmu/" dreamerone@ninmunanmu.com:~/ninmunanmu.com/
